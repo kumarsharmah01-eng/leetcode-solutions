407f985206314d1c1e9b8a66a422234509d42a1c
Harsh Kumar Sharma <kumarsharmah01@gmail.com>
2026-03-07 23:00:56 +0530

    Add threeSum function for finding triplets in array
    Implement threeSum function to find unique triplets that sum to zero.

---
+++ 3sum.cpp
@@ -0,0 +1,49 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].*/
+
+//solution 
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) 
+                continue;
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});    
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                }
+                else if(sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+         return ans;
+    }
+};
